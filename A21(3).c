bafb0ce066095682138af7720fabcdc2b33b1743
anumeena <111556950+anu6376081311@users.noreply.github.com>
2023-03-25 01:19:22 +0530

    Add files via upload

---
+++ A21(3).c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+struct Employee
+{
+    int id;
+    char name[30];
+    int salary;
+};
+int main()
+{
+    struct Employee e;
+    printf("Enter name of employee \n");
+    gets(e.name);
+    
+    printf("Enter name of id \n");
+    scanf("%d",&e.id);
+
+    printf("Enter salary \n");
+    scanf("%d",&e.salary);
+
+    printf("Name = %s,Id = %d,salary = %d",e.name, e.id, e.salary);
+    return 0;
+
+    }
